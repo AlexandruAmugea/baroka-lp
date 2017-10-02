c6d4a20340e12c7a36a785166805fe470a0da951
Alexandru Amugea <alexandru.amugea@endava.com>
2017-10-02 18:03:39 +0300

    Added interactios for phone section

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,5 +7,7 @@ if (module.hot) {
 import 'babel-polyfill';
 import '../styles/index.scss';
 import menu from './mobile-menu';
+import steps from './steps.js';
 
 menu.initMobileMenu();
+steps.initSteps();
---
+++ src/scripts/steps.js
@@ -0,0 +1,30 @@
+exports.initSteps = function initSteps(){
+    let stepsArray = document.getElementsByClassName('tile-steps');
+    let imagesArray = document.getElementsByClassName('image-tile');
+
+    this.removeAll = function(){
+        stepsArray[0].classList.remove('active');
+        stepsArray[1].classList.remove('active');
+        stepsArray[2].classList.remove('active');
+    };
+
+    this.switchImage = function(index){
+        imagesArray[0].classList.remove('active');
+        imagesArray[1].classList.remove('active');
+        imagesArray[2].classList.remove('active');
+        imagesArray[index].classList.add('active');
+    };
+
+    this.bindClick = (elem, index) => {
+        elem.addEventListener('click', () =>{
+            this.removeAll();
+            elem.classList.add('active');
+            this.switchImage(index);
+        });
+    };
+
+    this.bindClick(stepsArray[0], 0);
+    this.bindClick(stepsArray[1], 1);
+    this.bindClick(stepsArray[2], 2);
+
+};
