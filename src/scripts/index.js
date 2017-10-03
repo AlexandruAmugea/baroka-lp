36402fedc54dd884fd6f1924794682d8c7cf4df9
Alexandru Amugea <alexandru.amugea@endava.com>
2017-10-03 21:21:47 +0300

    Added form interaction angular cryptoicons

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,31 +9,52 @@ import '../styles/index.scss';
 import menu from './mobile-menu';
 import steps from './steps.js';
 import angular from 'angular';
+import dropdown from 'angular-ui-bootstrap/src/dropdown/index-nocss.js';
 
 menu.initMobileMenu();
 steps.initSteps();
 
 var app = angular.module('buroka', [
-  
+  dropdown
 ]);
 
 // Exchange form controller
 (function(){
   app.controller('exchangeFormCtrl', ['$scope', 'exchangeFactory', function($scope, exchangeFactory){
-    $scope.currency = {};
-
-    $scope.currency.selected = {
-      name: 'something', code: '321'
+    $scope.currency = {
+      sell: {
+        selected: {
+          'shortName': 'BTN', 
+          'name': 'Bitcoin',
+        }
+      },
+      get: {
+        selected: {
+          'shortName': 'USD', 
+          'name': 'US Dollar',
+        }
+      },
+      all: [
+        {'shortName': 'BTN', 'name': 'Bitcoin', 'imageClass': 'BTC'},
+        {'shortName': 'USD', 'name': 'US Dollar', 'imageClass': 'BTC'},
+        {'shortName': 'ETH', 'name': 'Ethereum', 'imageClass': 'ETH'},
+      ]
+    };
+    $scope.input = {
+      sell: 0,
+      get: 0
     };
 
-    $scope.currency.all = [
-      {'name': 1, 'code': '1'},
-      {'name': 2, 'code': '2'},
-      {'name': 3, 'code': '3'}
-    ];
+    $scope.changeSellCurrency = function(currency) {
+      $scope.currency.sell.selected = currency;
+    };
 
-    $scope.$watch('currency.sell', function(val){
-      $scope.currency.get = exchangeFactory.calculateCurrency($scope.currency.sell);
+    $scope.changeGetCurrency = function(currency) {
+      $scope.currency.get.selected = currency;
+    };
+
+    $scope.$watch('input.sell', function(val){
+      $scope.input.get = exchangeFactory.calculateCurrency(val);
     });
   }]);
 }());
