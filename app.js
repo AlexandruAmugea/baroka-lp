a35388535f4bcac6c471d1ec75e00001ef68d46e
Alexandru Amugea <alexandru.amugea@endava.com>
2017-11-22 19:05:33 +0200

    Added pages and modified configs

--- app.js
+++ app.js
@@ -13,4 +13,28 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + "/dist/index.html");
 })
 
+app.get('/faq', function(req, res){
+    res.sendFile(__dirname + "/dist/faq.html");
+})
+
+app.get('/privacy-policy', function(req, res){
+    res.sendFile(__dirname + "/dist/privacy-policy.html");
+})
+
+app.get('/refund', function(req, res){
+    res.sendFile(__dirname + "/dist/refund.html");
+})
+
+app.get('/risk-warning', function(req, res){
+    res.sendFile(__dirname + "/dist/risk-warning.html");
+})
+
+app.get('/terms', function(req, res){
+    res.sendFile(__dirname + "/dist/terms.html");
+})
+
+app.get('/anti-fraud-policy', function(req, res){
+    res.sendFile(__dirname + "/dist/anti-fraud-policy.html");
+})
+
 app.listen(port);
