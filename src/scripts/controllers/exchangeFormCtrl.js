fa15e2fa93e1f8363f5e64228f2c92cc282da734
Alexandru Amugea <alexandru.amugea@endava.com>
2017-12-15 18:07:52 +0200

    Exchange form filters adn timeout for requests

--- src/scripts/controllers/exchangeFormCtrl.js
+++ src/scripts/controllers/exchangeFormCtrl.js
@@ -6,43 +6,69 @@ function exchangeFormCtrl ($scope, $rootScope, exchangeFactory) {
       sell: 0,
       get: 0
     };
+    $scope.loading = false;
+    $scope.initFirstTime = true;
+    var timeForTypeInSeconds = 1500;
+    var formInput = document.getElementById('form-exchange-sell');
+    var numberRegex = new RegExp('^[0-9]$');
+    var typingTimer;
 
-    $scope.changeSellCurrency = function(currency) {
-      $scope.currency.sell.selected = currency;
+    var calculatePrice = function(){
       let pair = $scope.currency.sell.selected.shortName + '|' + $scope.currency.get.selected.shortName;
       exchangeFactory.calculateCurrency(pair, $scope.input.sell).then(function(r){
         $scope.input.get = r.data.ClientReceived;
+        $scope.loading = false;
       }, function(er){
         console.log(er);
       });
+    };
+
+    $scope.changeSellCurrency = function(currency) {
+      $scope.currency.sell.selected = currency;
+      calculatePrice();
     };
 
     $scope.changeGetCurrency = function(currency) {
       $scope.currency.get.selected = currency;
-      let pair = $scope.currency.sell.selected.shortName + '|' + $scope.currency.get.selected.shortName;
-      exchangeFactory.calculateCurrency(pair, $scope.input.sell).then(function(r){
-        $scope.input.get = r.data.ClientReceived;
-      }, function(er){
-        console.log(er);
-      });
+      calculatePrice();
     };
 
+    formInput.addEventListener('keydown', function(e){
+      if(numberRegex.test(e.key) || e.key === "Backspace") {
+        clearTimeout(typingTimer);
+      } else if(e.keyCode !== "ArrowRight" || e.keyCode !== "ArrowLeft") {
+        e.preventDefault();
+      }
+    });
+
+    formInput.addEventListener('keyup', function(e){
+      $scope.loading = true;
+      if(numberRegex.test(e.key) || e.key === "Backspace") {
+        clearTimeout(typingTimer);
+        typingTimer = setTimeout(calculatePrice, timeForTypeInSeconds);
+      } else if(e.keyCode != "ArrowRight" || e.keyCode != "ArrowLeft"){
+        e.preventDefault();
+      };
+    });
+
     $scope.$watch('input.sell', function(val){
-      let pair = $scope.currency.sell.selected.shortName + '|' + $scope.currency.get.selected.shortName;
-      exchangeFactory.calculateCurrency(pair, val).then(function(r){
-        $scope.input.get = r.data.ClientReceived;
-      }, function(er){
-        console.log(er);
-      });
+      if($scope.initFirstTime) {
+        $scope.initFirstTime = false;
+        return; 
+      } else if(val == 0 || val == ''){
+        $scope.input.get = 0;
+        return;
+      } else {
+        //calculatePrice();
+      }
     });
 
     $scope.openRegisterPopup = false;
-
     $scope.submit = function(){
       $rootScope.$emit('open:registerPopup');
     };
 
-    $scope.switchAmmounts = function(){
+    $scope.positons = function(){
       $scope.input.sell = $scope.input.get;
     };
 
--- src/scripts/factory/exchangeFactory.js
+++ src/scripts/factory/exchangeFactory.js
@@ -8,20 +8,6 @@ function exchangeFactory ($http, $q) {
   
       function calculateCurrency(pair, amount){
         let url = decodeURI('https://brka.space/api/sell/' + pair + '/' + amount + '/tfplf9jpvdsm0po7hvf8k03ncm9jjfzf');
-        // let promise = $q((resolve, reject) => {
-        //   var mockRes = {
-        //     data: {
-        //         "ClientValue": 1,
-        //         "ExchangeValue": 0.9,
-        //         "ExchangeTo": 16301.448,
-        //         "ClientReceived": 16300.448
-        //     }
-        //   };
-        //   resolve(mockRes);
-        // });
-
-        // return promise;
-
         return $http({
           method: 'GET',
           url: url
@@ -47,7 +33,7 @@ function exchangeFactory ($http, $q) {
             {'shortName': 'XRP', 'name': 'XRP', 'imageClass': 'XRP'},
             {'shortName': 'LTC', 'name': 'Litcoin', 'imageClass': 'LTC'},
             {'shortName': 'ETH', 'name': 'Ethereum', 'imageClass': 'ETH'},
-            {'shortName': 'USD', 'name': 'US Dollar', 'imageClass': 'BTC'}         
+            {'shortName': 'USD', 'name': 'US Dollar', 'imageClass': 'USDT'}         
           ]
         };
       }
